673e7611befc57f47ebc5948396dc618f66c2e6c
Vivek199012 <deptop725@gmail.com>
2024-06-19 12:32:28 +0200

    -

--- backend/core/account/serializers.py
+++ backend/core/account/serializers.py
@@ -3,7 +3,6 @@ from djoser.serializers import (
     UserSerializer as BaseUserSerializer,
 )
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
 
 User = get_user_model()
 
